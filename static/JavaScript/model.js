81e4ea5ede93222a268bdfb57363271e820a0c12
Victor Iliya <VictorIliya15@gmail.com>
2024-04-06 17:51:17 +0100

    corrected for the models: now its correct for SVM, KNN and Decision Tree

--- static/JavaScript/model.js
+++ static/JavaScript/model.js
@@ -3,8 +3,8 @@ document.getElementById("DT").addEventListener("click", function() {
     window.location.href = "/upload_DecisionTree";
 });
 
-document.getElementById("Forest").addEventListener("click", function() {
-    window.location.href = "/upload_RandomForest";
+document.getElementById("KNN").addEventListener("click", function() {
+    window.location.href = "/upload_KNN";
 });
 
 document.getElementById("SVM").addEventListener("click", function() {
