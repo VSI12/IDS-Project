4fb7159c433e0e833afbacd398e4977da97c2757
Victor Iliya <VictorIliya15@gmail.com>
2024-10-31 13:38:37 +0100

    s3 upload

---
+++ ids-frontend/src/app/api/s3-upload/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+
+const s3client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+})
+
+
+async function uploadFileToS3(file, fileName) {
+    
+    const fileBuffer = file;
+    console.log(fileName);
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${fileName}-${Date.now()}`,
+        Body: fileBuffer,
+        ContentType: 'text/csv'
+    };
+
+}
+
+
+
+
+
+
+
+export async function POST(request) {
+
+    try {
+
+        const formData = await request.formData();
+        const file = formData.get("file");
+
+        if(!file) {
+            return NextResponse.json({ error: "File is required" }, {status:400});
+        }
+
+        const buffer = Buffer.from(await file.arrayBuffer());
+        const fileName = await uploadFileToS3(buffer, file.name);
+
+        return NextResponse.json({ success: true, fileName });
+
+    } catch (error) {
+    return NextResponse.json({ error: "Failed to upload file" });
+    }
+}
