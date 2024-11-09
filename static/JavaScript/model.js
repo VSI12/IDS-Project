6d180eb46e5d4fdbf82b7bd5d5186d181bf118fd
Victor Iliya <VictorIliya15@gmail.com>
2024-11-09 12:55:40 +0100

    edited the html and js files to include the random forest model option

--- static/JavaScript/model.js
+++ static/JavaScript/model.js
@@ -7,6 +7,11 @@ document.getElementById("KNN").addEventListener("click", function() {
     window.location.href = "/upload_KNN";
 });
 
-document.getElementById("NaiveBayes").addEventListener("click", function() {
+document.getElementById("RF").addEventListener("click", function() {
+    window.location.href = "/upload_RandomForest";
+});
+
+document.getElementById("NB").addEventListener("click", function() {
     window.location.href = "/upload_NaiveBayes";
 });
+
